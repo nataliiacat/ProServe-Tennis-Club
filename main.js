80ba50896088ddee4c0ca35066db0cda5ee657b0
nataliiacat <nataliagricaj6@gmail.com>
2024-12-07 05:39:10 +0100

    Add services slider, join section, and footer. Implement responsive design.

--- main.js
+++ main.js
@@ -5,7 +5,34 @@ const menuLinks = document.querySelectorAll(".header__link");
 const showPopupBtn = document.querySelector(".book-now-btn");
 const formPopup = document.querySelector(".form-popup");
 const hidePopupBtn = document.querySelector(".form-popup .close-btn");
-
+let swiperCards = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1920: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    480: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    100: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    }
+  }
+});
 showPopupBtn.addEventListener("click", () => {
   document.body.classList.toggle("show-popup");
 });
